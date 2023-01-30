941038d0684c4dfa808e3a9af7dc9b0e92bd9aa7
VISHRUTHI-K <123851287+VISHRUTHI-K@users.noreply.github.com>
2023-01-30 22:12:49 +0530

    Create maximum_minimum.c

---
+++ maximum_minimum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a=3,b=4,c=5,max,min;
+max=(a>=b&&a>=c)?a:(b<=a&&b<=c)?b:c;
+min=(a<=b&&a<=c)?a:(b<=a&&b<=c)?b:c;
+printf("the maximum and minimum are%d%d respectively",max,min);
+return;
+}
+
+
+OUTPUT:
+the maximum and minimum are 5 3 respectively
