fcf7aa43decea61e62833eed4ad5fa1df563ba31
VISHRUTHI-K <123851287+VISHRUTHI-K@users.noreply.github.com>
2023-01-30 22:29:03 +0530

    Create roots-of_quadraticequation.c

---
+++ roots-of_quadraticequation.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c,d,root1,root2;
+printf("enter values of a,b,c");
+scanf("%d%d%d",&a,&b,&c);
+d=b*b-4*a*c;
+if(d>0)
+{
+root1=(-b+sqrt(d))/(2*a);
+root2=(-b-sqrt(d))/(2*a);
+printf("%d%d",root1,root2);
+printf("roots are real");
+}
+else if(d==0)
+{
+root1=-b/(2*a);
+root2=-b/(2*a);
+printf("%d%d",root1,root2);
+printf("roots are equal");
+}
+else
+printf("roots are imaginary");
+return;
+}
