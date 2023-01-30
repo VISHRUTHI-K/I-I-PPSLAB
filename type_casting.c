7e33f67709336a9bad38ac7de092ef263eb06909
VISHRUTHI-K <123851287+VISHRUTHI-K@users.noreply.github.com>
2023-01-30 22:09:16 +0530

    Create type_casting.c

---
+++ type_casting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+
+float c,d;
+printf("enter the values of a and b")'
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%f\n",c);
+d=(float)a/b;
+printf("%f",d);
+return;
+}
+
+OUTPUT:
+enter the values of a and b 6 4
+  1.000000
+  1.500000
